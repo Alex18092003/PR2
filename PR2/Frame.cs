60e715d61cdefb5d4e04c17b190847fc38bdf4c0
лебедевааф <@106-06>
2022-10-07 18:16:41 +0300

    Добавьте файлы проекта.

---
+++ PR2/Frame.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace PR2
+{
+    internal class Frame
+    {
+        public static Frame MainFrame;
+    }
+}
