9190c741a75cb6e07497813977ebb85f860ae6d4
Саша Лебедева <sashyla1809@gmail.com>
2022-11-03 14:49:22 +0300

    Почти вся 7 лаба

---
+++ PR2/Classes/PartialEntryClass.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace PR2
+{
+    DateTime d = DateTime.Now;
+    public partial class Entry
+    {
+
+        public string Birthday
+        {
+            get
+            {
+                return Date.ToString("dd MMMM yyyy года");
+            }
+        }
+
+        public SolidColorBrush DateColor
+        {
+            get
+            {
+
+
+            }
+        }
+
+    }
+}
